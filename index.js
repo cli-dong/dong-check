fb86c5d96943d9cc517545af428538a7bd5bec95
crossjs <liwenfu@crossjs.com>
2015-06-09 19:00:04 +0800

    update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ module.exports = {
   options: [{
     name: 'src',
     alias: 's',
-    description: '文件路径，支持 glob',
+    description: '文件路径，支持 glob（以空格分隔多个）',
     defaults: '**/*.js'
   }, {
     name: 'threshold',
@@ -22,5 +22,13 @@ module.exports = {
     description: '最大允许显示的错误数量',
     defaults: 10
   }],
-  bootstrap: require('./lib/check')
+  bootstrap: require('./lib/check'),
+  help: function(chalk) {
+    console.log('  Examples:')
+    console.log('')
+    console.log(chalk.gray('    $ ') +
+                chalk.magenta('dong check -s "app/**/*.js mod/**/*.js"') +
+                chalk.gray(' ....... check js files in app and mod'))
+    console.log('')
+  }
 }
